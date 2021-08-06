49f7a00c92c2d2958904d153aea0393ed1e6dd3d
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 11:43:58 +0300

    Added movie detail view and url for it

--- movies/urls.py
+++ movies/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from movies.views import Movies
+from movies.views import Movies, Movie
 
 urlpatterns = [
     path('', Movies.as_view(), name='home'),
+    path('movie/<slug:slug>', Movie.as_view(), name='movie'),
 ]
 
 if settings.DEBUG:
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from movies.models import Movie
 
 
 class Movies(ListView):
+    model = Movie
     queryset = Movie.objects.all()
     context_object_name = 'movies'
+
+
+class Movie(DetailView):
+    model = Movie
+    slug_field = 'slug'
