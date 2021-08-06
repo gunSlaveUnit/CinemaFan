e223a39370738170a5e6e629bf5ddf5e1542b7d5
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 12:29:43 +0300

    Added automatic slug field

--- movies/admin.py
+++ movies/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from movies.models import Movie
 
-admin.site.register(Movie)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
