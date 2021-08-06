2d8615bf8560c27dad57a4f329b2ce80ba1b34a5
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 09:14:43 +0300

    Added UpdateUserForm

--- users/forms.py
+++ users/forms.py
@@ -13,3 +13,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+class UpdateUserForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=False,
+                                 help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False,
+                                help_text='Optional')
+    email = forms.EmailField(max_length=254, help_text='Inform a valid email address')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
