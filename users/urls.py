8488921fb7ef6cb66d5df8782c3fe5fb7f35c7ad
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 18:55:28 +0300

    Added url for password reset

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from django.urls import path
 
 from users.views import signup, profile
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signin', LoginView.as_view(template_name='users/signin.html'), name='signin'),
     path('signout', LogoutView.as_view(template_name='users/signout.html'), name='signout'),
     path('profile', profile, name='profile'),
+    path('password-reset', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
 ]
