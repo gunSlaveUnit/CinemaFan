dda78b6c70aaf548b2e5b9d6701f1ef13c417f2d
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 09:15:57 +0300

    Added UpdateProfileForm

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from users.models import Profile
+
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False,
@@ -25,3 +27,9 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('image')
