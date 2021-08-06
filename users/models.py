995b7998429cc8adf01368402a7a3fa5b4ab7dbc
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 09:51:06 +0300

    Fixed problem with image opening and attribute "height"

--- users/models.py
+++ users/models.py
@@ -13,8 +13,8 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.url)
-        if img.heigth > 300 or img.width > 300:
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.url)
+            img.save(self.image.path)
