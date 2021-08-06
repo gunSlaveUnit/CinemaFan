159110707c92a58ae267958c2bb013c3719b890f
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 19:29:52 +0300

    Added url for password reset complete and a template for it

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
-    PasswordResetConfirmView
+    PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 
 from users.views import signup, profile
@@ -10,9 +10,12 @@ urlpatterns = [
     path('signout', LogoutView.as_view(template_name='users/signout.html'), name='signout'),
     path('profile', profile, name='profile'),
     path('password-reset', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    path('password-reset-done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(
         template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('password-reset-complete', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
