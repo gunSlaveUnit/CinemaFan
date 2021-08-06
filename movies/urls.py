917b2b17ee5f121c68b88a05928d64607dd09125
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-06 12:47:22 +0300

    Added url for create movie view

--- movies/urls.py
+++ movies/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from movies.views import Movies, Movie
+from movies.views import Movies, Movie, CreateMovie
 
 urlpatterns = [
     path('', Movies.as_view(), name='home'),
     path('movie/<slug:slug>', Movie.as_view(), name='movie'),
+    path('movie/create', CreateMovie.as_view(), name='movie_create'),
 ]
 
 if settings.DEBUG:
