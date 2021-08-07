c6a41bf1f1f5aa512cb4759e83aba10fa8f3c152
GunSlaveUnit <rp34789t5409yvt@protonmail.com>
2021-08-07 23:50:39 +0300

    Added cache for the Moviews and Moview views

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from movies.views import Movies, MovieDetail, MovieCreate, MovieUpdate, MovieDelete
 
 urlpatterns = [
-    path('', Movies.as_view(), name='home'),
-    path('movie/<slug:slug>', MovieDetail.as_view(), name='movie'),
+    path('', cache_page(60)(Movies.as_view()), name='home'),
+    path('movie/<slug:slug>', cache_page(60)(MovieDetail.as_view()), name='movie'),
     path('create', MovieCreate.as_view(), name='movie_create'),
     path('movie/<slug:slug>/update', MovieUpdate.as_view(), name='movie_update'),
     path('movie/<slug:slug>/delete', MovieDelete.as_view(), name='movie_delete'),
